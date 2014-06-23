3699c9abcfe0498ac1a43bea29707c880412b45f
Joe <josephdburdick@gmail.com>
2014-06-23 16:30:15 -0400

    Initial commit. Updated Bootstrap CSS to v3.1.1

---
+++ MVC-Bootstrap2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Bootstrap2.MvcApplication" Language="C#" %>
---
+++ MVC-Bootstrap2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Bootstrap2.Startup))]
+namespace MVC_Bootstrap2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
